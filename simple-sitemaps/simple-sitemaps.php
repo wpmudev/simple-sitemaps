1bb1328f66ddf43a652d2c5f485f325c0787b4c2
vladislav <ve@incsub.com>
2017-08-22 11:01:03 +0200

    Refactor update notifications notice

--- simple-sitemaps/simple-sitemaps.php
+++ simple-sitemaps/simple-sitemaps.php
@@ -56,8 +56,27 @@ class Incsub_SimpleSitemaps {
 		add_action( 'publish_post', array(&$this, 'PingSearchEngines'), 16 );
     	add_action( 'delete_post', array(&$this, 'PingSearchEngines'), 16 );
 
-    	$totalposts = defined('SIMPLE_SITEMAPS_POST_SOFT_LIMIT') ? SIMPLE_SITEMAPS_POST_SOFT_LIMIT : $this->totalposts;
+		add_action( 'admin_init', array($this, 'init_dashboard') );
+
+		$totalposts = defined('SIMPLE_SITEMAPS_POST_SOFT_LIMIT')
+			? SIMPLE_SITEMAPS_POST_SOFT_LIMIT
+			: $this->totalposts
+		;
     	$this->totalposts = apply_filters('simple_sitemaps-totals_soft_limit', $totalposts);
+	}
+
+	public function init_dashboard () {
+		$dash = dirname(__FILE__) . '/dash-notice/wpmudev-dash-notification.php';
+		if (file_exists($dash)) {
+			global $wpmudev_notices;
+			if (!is_array($wpmudev_notices)) $wpmudev_notices = array();
+			$wpmudev_notices[] = array(
+				'id' => 39,
+				'name' => 'Simple Sitemaps For Multisite',
+				'screens' => array(),
+			);
+			require_once($dash);
+		}
 	}
 
 	// Delete this blog's sitemap
@@ -264,17 +283,6 @@ if (!function_exists('file_put_contents')) {
 }
 
 
-///////////////////////////////////////////////////////////////////////////
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-  }
-}
-/* --------------------------------------------------------------------- */
 
 ///////////////////////////////////////////////////////////////////////////
 /* -------------------- File required Notice -------------------- */
